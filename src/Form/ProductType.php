12a73077ef2304b9cd0bcaecb1e11b84a99499ab
Michael ALTMAN <altman_mike@yahoo.fr>
2026-01-06 11:13:15 +0100

    ADD Bootrap classes in form

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -2,32 +2,68 @@
 
 namespace App\Form;
 
-use App\Entity\Product;
 use App\Entity\Tag;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use App\Entity\Product;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 
 class ProductType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('price')
-            ->add('stock')
+            ->add('name', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Name product'
+                ],
+                'label' => 'Name',
+                'label_attr' => ['class' => 'my-2']
+            ])
+            ->add('price', MoneyType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Price product'
+                ],
+                'label' => 'Price product',
+                'label_attr' => ['class' => 'my-2']
+            ])
+            ->add('stock', NumberType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Product stock number'
+                ],
+                'label' => 'Stock',
+                'label_attr' => ['class' => 'my-2']
+            ])
             ->add('createdAt', null, [
+                'attr' => [
+                    'class' => 'form-control'                    
+                ],
                 'widget' => 'single_text',
+                'label_attr' => ['class' => 'my-2']
             ])
             ->add('updatedAt', null, [
+                'attr' => [
+                    'class' => 'form-control'                    
+                ],
                 'widget' => 'single_text',
+                'label_attr' => ['class' => 'my-2']
             ])
-            ->add('tags', EntityType::class, [
+            ->add('tags', EntityType::class, [                
+                'attr' => [
+                    'class' => 'form-control'                    
+                ],
                 'class' => Tag::class,
                 'choice_label' => 'id',
                 'multiple' => true,
-                'expanded' => true
+                'expanded' => true,
+                'label_attr' => ['class' => 'my-2']
             ])
         ;
     }
